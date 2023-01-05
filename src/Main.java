c7f1b9b1154de91ff6df30886b974d553921e8e2
orentas <o.b.ornetas@gmail.com>
2023-01-05 14:04:25 +0200

    CRUDo konstravimas, pradzia

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> popCars = new ArrayList<>();
+        popCars.add("Audi");
+        popCars.add("BMW");
+        popCars.add("VW");
+        popCars.add("Ford");
+
+        System.out.println("Most popular cars the year");
+        System.out.println("Choose from following");
+        System.out.println("1. See contestants");
+        System.out.println("2. Add contestant");
+        System.out.println("3. Edit contestant");
+        System.out.println("4. Delete contestant");
+        System.out.println("5. Exit");
+        Scanner sc = new Scanner(System.in);
+
+
+
+
     }
 }
